145a52d258fa7217eb8bd5d4937dcf1be1929ecb
Mbulelo <mbulelopani09@gmail.com>
2021-02-05 15:52:21 +0200

    Updates

---
+++ precache-manifest.4331ec93cbfcb60f9475394d29f92094.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddc2066540ff7d763aeec8c82b75b3a6",
+    "url": "/my-portfolio2/index.html"
+  },
+  {
+    "revision": "17bd5891bd28bc47d8de",
+    "url": "/my-portfolio2/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "9a532425385ba2b52c43",
+    "url": "/my-portfolio2/static/css/main.603f79fc.chunk.css"
+  },
+  {
+    "revision": "17bd5891bd28bc47d8de",
+    "url": "/my-portfolio2/static/js/2.8a921379.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/my-portfolio2/static/js/2.8a921379.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a532425385ba2b52c43",
+    "url": "/my-portfolio2/static/js/main.b2fef52d.chunk.js"
+  },
+  {
+    "revision": "2e8bc87790d47ab8f816",
+    "url": "/my-portfolio2/static/js/runtime-main.95b1a28b.js"
+  },
+  {
+    "revision": "61adf21a0d1383edfd21468da64a7efc",
+    "url": "/my-portfolio2/static/media/fadebck.61adf21a.jpg"
+  },
+  {
+    "revision": "8e3f080bd1dafcda706e0f709141d3dc",
+    "url": "/my-portfolio2/static/media/tech1.8e3f080b.jpg"
+  }
+]);
